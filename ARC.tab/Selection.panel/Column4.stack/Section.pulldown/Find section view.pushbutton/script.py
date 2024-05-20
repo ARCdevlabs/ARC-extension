9766c7c469eedea568753f16e4868c9a4f7b9a22
Nguyễn Thanh Sơn <ARCdevlabs@gmail.com>
2024-05-20 15:29:29 +0700

    Update script.py

--- ARC.tab/Selection.panel/Column4.stack/Section.pulldown/Find section view.pushbutton/script.py
+++ ARC.tab/Selection.panel/Column4.stack/Section.pulldown/Find section view.pushbutton/script.py
@@ -9,7 +9,6 @@ module = importlib.import_module(str(begin))
 import Autodesk
 from Autodesk.Revit.DB import *
 from System.Collections.Generic import *
-print "a"
 try:
     if module.AutodeskData():
         uidoc = __revit__.ActiveUIDocument
