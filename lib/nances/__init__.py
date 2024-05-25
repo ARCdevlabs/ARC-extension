482efbfd0989ae7f8038b6a27918fb42e565fcde
nguyenthanhson1712 <ARCdevlabs@gmail.com>
2024-05-25 16:20:48 +0700

    Modify dim

--- lib/nances/__init__.py
+++ lib/nances/__init__.py
@@ -854,20 +854,22 @@ PYREVIT_VERSION_APP_DIR = op.join(PYREVIT_APP_DIR)
 uidoc = __revit__.ActiveUIDocument
 doc = uidoc.Document
 # Def nay cho current element
-def get_selected_elements(tem_uidoc, tem_doc):
+def get_selected_elements(tem_uidoc, tem_doc, noti = True):
     selection = tem_uidoc.Selection
     selection_ids = selection.GetElementIds()
     elements = []
     for element_id in selection_ids:
         elements.append(tem_doc.GetElement(element_id))
+
     if len(elements) == 0:
-        from Autodesk.Revit.UI import TaskDialog
-        # module_path = Autodesk.__file__
-        # print module_path
-        dialog = TaskDialog("ARC: Thông báo")
-        dialog.MainContent = "Phải chọn trước các đối tượng rồi mới click vào tool."
-        dialog.TitleAutoPrefix = False
-        dialog.Show()
+        if noti:
+            from Autodesk.Revit.UI import TaskDialog
+            # module_path = Autodesk.__file__
+            # print module_path
+            dialog = TaskDialog("ARC")
+            dialog.MainContent = "Please select element before use this tool"
+            dialog.TitleAutoPrefix = False
+            dialog.Show()
         return False
     else: 
         return elements
@@ -1409,3 +1411,185 @@ clr.AddReference("System.Windows.Forms")
 from System.Windows.Forms import MessageBox, MessageBoxButtons, MessageBoxIcon
 def message_box(message):
     MessageBox.Show(message, "ARC: Notification", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+
+
+
+# math
+import Autodesk
+from Autodesk.Revit.DB import *
+import Autodesk.Revit.DB as DB
+from System.Collections.Generic import *
+
+def get_nearest_point(points, reference_point):
+
+    min_distance = float('inf')
+    nearest_point = None
+    
+    for point in points:
+        distance = point.DistanceTo(reference_point)
+        if distance < min_distance:
+            min_distance = distance
+            nearest_point = point
+    return nearest_point
+
+
+def distance_2_point(point , reference_point):
+    distance = point.DistanceTo(reference_point)
+    return distance
+
+def xac_dinh_kich_co_chu (view,value, one_unit_width = 1.85): #don vi phai la mm
+    round_format_value = round(value,2)
+    formatted_value = str(round_format_value).rstrip('0').rstrip('.') #don vi mm
+    len_formatted_value = len(formatted_value) #co bao nhieu ky tu, vi du 200 in ra la 3 
+    width_text = float(len_formatted_value * one_unit_width * (view.Scale)) #don vi mm trong moi truong view (khong phai ban ve)
+    return width_text
+
+
+def move_point_along_vector(point, vector, distance):
+    new_point = point + vector.Normalize() * distance
+    return new_point
+
+
+
+def add_prefix_to_dimension(dimension):
+    try:
+        dimension.Prefix = "W="
+    except:
+        pass    
+
+def orientation_mat_bang(A, B, vector):
+    C = move_point_along_vector(B, vector, 1)
+    # Chuyển đổi tọa độ thành tuple
+    # Vector AB
+    vector_AB = (B.X - A.X, B.Y - A.Y)
+    # Vector BC
+    vector_BC = (C.X - B.X, C.Y - B.Y)
+    # Tính cross product
+    cross_product = vector_AB[0] * vector_BC[1] - vector_AB[1] * vector_BC[0]
+    # Xác định hướng dựa trên dấu của cross product
+    if cross_product > 0:
+        ket_qua = "Bên trái"
+    elif cross_product < 0:
+        ket_qua = "Bên phải"
+    else:
+        ket_qua= "Thẳng hàng"        
+    return ket_qua
+
+def orientation_mat_cat(A, B, vector):
+    C = move_point_along_vector(B, vector, 1)
+    # Chuyển đổi tọa độ thành tuple
+    # Vector AB
+    vector_AB = (B.Z - A.Z, B.Y - A.Y)
+    # Vector BC
+    vector_BC = (C.Z - B.Z, C.Y - B.Y)
+    # Tính cross product
+    cross_product = vector_AB[0] * vector_BC[1] - vector_AB[1] * vector_BC[0]
+    # Xác định hướng dựa trên dấu của cross product
+    if cross_product > 0:
+        ket_qua = "Bên trái"
+    elif cross_product < 0:
+        ket_qua = "Bên phải"
+    else:
+        ket_qua= "Thẳng hàng"        
+    return ket_qua
+
+
+def distance_mat_bang(point1, point2):
+    return ((point2.X - point1.X)**2 + (point2.Y - point1.Y)**2)**0.5
+
+def distance_mat_cat(point1, point2):
+    return ((point2.Z - point1.Z)**2 + (point2.Y - point1.Y)**2)**0.5
+
+
+def distance_from_point_to_element_mat_bang(point1, obj):
+    return distance_mat_bang(point1, obj.Origin)
+
+def distance_from_point_to_element_mat_cat(point1, obj):
+    return distance_mat_cat(point1, obj.Origin)
+
+def sort_points_by_distance(A, points_list):
+    # Sắp xếp các điểm trong list dựa trên khoảng cách từ xa đến gần điểm A
+    sorted_points = sorted(points_list, key=lambda point: distance_mat_bang(A, point), reverse=True)
+    return sorted_points
+
+
+def sort_seg_by_distance_mat_bang(A, seg_list):
+    # Sắp xếp các điểm trong list dựa trên khoảng cách từ xa đến gần điểm A
+    sorted_points = sorted(seg_list, key=lambda obj: distance_from_point_to_element_mat_bang(A, obj), reverse=True)
+    return sorted_points
+
+def sort_seg_by_distance_mat_cat(A, seg_list):
+    # Sắp xếp các điểm trong list dựa trên khoảng cách từ xa đến gần điểm A
+    sorted_points = sorted(seg_list, key=lambda obj: distance_from_point_to_element_mat_cat(A, obj), reverse=True)
+    return sorted_points
+
+
+def move_segment_xa_nhat (list_sorted, vector_cua_dim, kich_co_chu, khoang_cach_dim_toi_text, huong_phai = True):
+
+    seg_xa_nhat= list_sorted[0]
+
+    value_segment = seg_xa_nhat.Value
+    
+    vi_tri = seg_xa_nhat.Origin
+
+    cong_thuc = ((kich_co_chu/304.8)/2) + ((value_segment)/2) #theo don vi feet
+    cong_thuc = ((kich_co_chu/304.8)/2) + ((value_segment)/2) + khoang_cach_dim_toi_text
+    if huong_phai:
+        move = move_point_along_vector(vi_tri, vector_cua_dim, cong_thuc) #move theo don vi feet
+    else:
+        move = move_point_along_vector(vi_tri, -vector_cua_dim, cong_thuc) #move theo don vi feet
+    
+    seg_xa_nhat.TextPosition = move
+
+    return 
+
+def chuan_hoa_vector_mat_bang(vector): #vector tu trai qua phai, tu duoi len tren
+    point_start = DB.XYZ(0,0,0)
+    point_end = move_point_along_vector(point_start, vector, 1)
+
+    if abs(vector.X) < abs(vector.Y):
+        if point_start.Y < point_end.Y:
+            return vector          
+        else:
+            return - vector
+    elif point_start.X < point_end.X:
+        return vector 
+    else: 
+        return - vector
+    
+def chuan_hoa_vector_mat_cat(vector): #vector tu trai qua phai, tu duoi len tren
+    point_start = DB.XYZ(0,0,0)
+    point_end = move_point_along_vector(point_start, vector, 1)
+
+    if abs(vector.Z) < abs(vector.Y):
+        if point_start.Y < point_end.Y:
+            return vector          
+        else:
+            return - vector
+    elif point_start.Z < point_end.Z:
+        return vector 
+    else: 
+        return - vector
+    
+def pick_point_with_nearest_snap(iuidoc):       
+    snap_settings = UI.Selection.ObjectSnapTypes.None
+    prompt = "Click"
+    try:
+        click_point = iuidoc.Selection.PickPoint(snap_settings, prompt)
+    except:
+        # print(traceback.format_exc())
+        pass
+    return click_point
+
+def set_work_plane_for_view(view):
+    current_work_plane = view.SketchPlane
+    if current_work_plane is None:
+        sketch_plane = view.SketchPlane
+        try:
+            sketch_plane = Autodesk.Revit.DB.SketchPlane.Create(view.Document, Autodesk.Revit.DB.Plane.CreateByNormalAndOrigin(view.ViewDirection, view.Origin))
+            view.SketchPlane = sketch_plane
+        except:
+            pass
+    return True
