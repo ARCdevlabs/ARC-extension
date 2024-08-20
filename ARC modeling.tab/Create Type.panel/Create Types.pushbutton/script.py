34c0b81db08d3429d23a4a0f0922e52aa7cdd8d5
ARCdevlabs <ARCdevlabs@gmail.com>
2024-08-20 09:48:35 +0700

    s
    đa

--- ARC modeling.tab/Create Type.panel/Create Types.pushbutton/script.py
+++ ARC modeling.tab/Create Type.panel/Create Types.pushbutton/script.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from pyrevit import HOST_APP, UI
+from nances import HOST_APP, UI
 from Autodesk.Revit.UI import UIApplication, RevitCommandId, PostableCommand
 uiapp = HOST_APP.uiapp
 uiapp.PostCommand(UI.RevitCommandId.LookupCommandId("CustomCtrl_%CustomCtrl_%Add-Ins%Create and Update Type%Create and Update Type"))
