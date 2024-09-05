11e82fe9efeab34277421c812148b1b79194a23e
Nguyễn Thanh Sơn <ARCdevlabs@gmail.com>
2024-09-05 13:01:40 +0700

    Tool Export Layer

--- ARC modeling.tab/Create Type.panel/Create Types.pushbutton/script.py
+++ ARC modeling.tab/Create Type.panel/Create Types.pushbutton/script.py
@@ -1,5 +1,9 @@
 # encoding: utf-8
-from nances import HOST_APP, UI
+from nances import HOST_APP, UI, message_box
 from Autodesk.Revit.UI import UIApplication, RevitCommandId, PostableCommand
-uiapp = HOST_APP.uiapp
-uiapp.PostCommand(UI.RevitCommandId.LookupCommandId("CustomCtrl_%CustomCtrl_%Add-Ins%Create and Update Type%Create and Update Type"))
+try:
+    uiapp = HOST_APP.uiapp
+    uiapp.PostCommand(UI.RevitCommandId.LookupCommandId("1CustomCtrl_%CustomCtrl_%Add-Ins%Create and Update Type%Create and Update Type"))
+except:
+    message_box("Hãy cài AddIn Tạo type của M.Cường để dùng được tool này. "
+                "Liên hệ Sơn hoặc M.Cường để biết cách sử dụng.")
