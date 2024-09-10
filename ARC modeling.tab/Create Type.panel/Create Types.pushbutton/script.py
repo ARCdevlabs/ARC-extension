78a05e6815543e71d782050ea26e0862f1f23cb5
Nguyễn Thanh Sơn <ARCdevlabs@gmail.com>
2024-09-10 10:44:40 +0700

    Sửa tool copy móng

--- ARC modeling.tab/Create Type.panel/Create Types.pushbutton/script.py
+++ ARC modeling.tab/Create Type.panel/Create Types.pushbutton/script.py
@@ -3,7 +3,7 @@ from nances import HOST_APP, UI, message_box
 from Autodesk.Revit.UI import UIApplication, RevitCommandId, PostableCommand
 try:
     uiapp = HOST_APP.uiapp
-    uiapp.PostCommand(UI.RevitCommandId.LookupCommandId("1CustomCtrl_%CustomCtrl_%Add-Ins%Create and Update Type%Create and Update Type"))
+    uiapp.PostCommand(UI.RevitCommandId.LookupCommandId("CustomCtrl_%CustomCtrl_%Add-Ins%Create and Update Type%Create and Update Type"))
 except:
     message_box("Hãy cài AddIn Tạo type của M.Cường để dùng được tool này. "
                 "Liên hệ Sơn hoặc M.Cường để biết cách sử dụng.")
