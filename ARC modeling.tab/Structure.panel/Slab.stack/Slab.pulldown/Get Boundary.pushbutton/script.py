1642b985ffd6671aa66b9e20b5bd8de4dd32047f
Nguyễn Thanh Sơn <ARCdevlabs@gmail.com>
2024-10-22 15:32:59 +0700

    Update tool tạo cách nhiệt

--- ARC modeling.tab/Structure.panel/Slab.stack/Slab.pulldown/Get Boundary.pushbutton/script.py
+++ ARC modeling.tab/Structure.panel/Slab.stack/Slab.pulldown/Get Boundary.pushbutton/script.py
@@ -17,30 +17,50 @@ if module.AutodeskData():
     Ele = module.get_element(uidoc,doc, "Select Floors to get boundary", noti = False)
     select = uidoc.Selection
     detailline= []
-    a = Ele[0]
-    ref = HostObjectUtils.GetTopFaces(a)
+    mot_san = Ele[0]
+    ref = HostObjectUtils.GetBottomFaces(mot_san)
     try:
         view_direction = Currentview.ViewDirection
         if view_direction.Z == 1 and str(Currentview.ViewType) != "ThreeD":
-            t = Transaction (doc, "Get Floor's Boundary")
-            t.Start()
+            # param_bottom_offset = mot_san.get_Parameter(DB.BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM)
+            param_bottom_offset = module.get_builtin_parameter_by_name(mot_san, DB.BuiltInParameter.STRUCTURAL_ELEVATION_AT_BOTTOM)
+            bottom_level = param_bottom_offset.AsDouble()
             for i in ref:
-                try:
-                    boundaryloops = a.GetGeometryObjectFromReference(i).GetEdgesAsCurveLoops()
-                    for loop in boundaryloops:   
-                        sketchloop = []
-                        sketchloop.append([x for x in loop])
-                        for a1 in sketchloop:
-                            for a2 in a1:
-                                Startpoint = a2.GetEndPoint(0)
-                                Endpoint = a2.GetEndPoint(1)
-                                PlaPoint1 = XYZ(Startpoint.X, Startpoint.Y, 0)
-                                PlaPoint2 = XYZ(Endpoint.X, Endpoint.Y, 0)
-                                L1 = Line.CreateBound(PlaPoint1,PlaPoint2)
-                                detailline.append(doc.Create.NewDetailCurve(Currentview,L1))
-                except:
-                    pass
-            t.Commit()
+                t = Transaction (doc, "Get Floor's Boundary")
+                t.Start()
+                if param_bottom_offset.HasValue == False:
+                    try:
+                        boundaryloops = mot_san.GetGeometryObjectFromReference(i).GetEdgesAsCurveLoops()
+                        for loop in boundaryloops:   
+                            sketchloop = []
+                            sketchloop.append([x for x in loop])
+                            for a1 in sketchloop:
+                                for a2 in a1:
+                                    Startpoint = a2.GetEndPoint(0)
+                                    Endpoint = a2.GetEndPoint(1)
+                                    PlaPoint1 = XYZ(Startpoint.X, Startpoint.Y, 0)
+                                    PlaPoint2 = XYZ(Endpoint.X, Endpoint.Y, 0)
+                                    L1 = Line.CreateBound(PlaPoint1,PlaPoint2)
+                                    detailline.append(doc.Create.NewDetailCurve(Currentview,L1))
+
+                    except:
+                        # import traceback
+                        # print(traceback.format_exc())
+                        pass
+                else:
+                    try:
+                        boundaryloops = mot_san.GetGeometryObjectFromReference(i).GetEdgesAsCurveLoops()
+                        for loop in boundaryloops:   
+                            sketchloop = []
+                            sketchloop.append([x for x in loop])
+                            for a1 in sketchloop:
+                                for a2 in a1:
+                                    detailline.append(doc.Create.NewDetailCurve(Currentview,a2))
+                    except:
+                        # import traceback
+                        # print(traceback.format_exc())
+                        pass
+                t.Commit()    
             listid = []
             for seg in detailline:
                 segid = seg.Id
@@ -50,6 +70,8 @@ if module.AutodeskData():
         else:
             module.message_box("Please use the tool in plan view.") 
     except:
+        # import traceback
+        # print(traceback.format_exc())
         pass
 
 
