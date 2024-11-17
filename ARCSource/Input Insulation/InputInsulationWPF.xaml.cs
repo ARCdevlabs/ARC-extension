66f762c9354af6eeb2ae4f91324977d66ed66c75
ARCdevlabs <ARCdevlabs@gmail.com>
2024-11-17 21:49:04 +0700

    update
    s

--- ARCSource/Input Insulation/InputInsulationWPF.xaml.cs
+++ ARCSource/Input Insulation/InputInsulationWPF.xaml.cs
@@ -8,17 +8,25 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
 
+
+
 namespace Input_Insulation
 {
     public partial class MainWindow : Window
     {
-
+        
         public MainWindow()
         {
             InitializeComponent();
-            
+            Cbb_SelectTypeInsulation.Items.Add(Utility.TypeOfInsulation.けいカル);
+            Cbb_SelectTypeInsulation.Items.Add(Utility.TypeOfInsulation.巻き付け);
+            Cbb_SelectTypeInsulation.SelectedIndex = 0;
         }
 
+        private void ClickButtonInsulationColumn(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 
 }
