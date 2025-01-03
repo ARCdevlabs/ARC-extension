8fbfe61ddddb388092318931482ececdc99d3155
Nguyễn Thanh Sơn <ARCdevlabs@gmail.com>
2025-01-03 16:53:35 +0700

    Update

--- ARC drawing.tab/Anotate.panel/Tool.stack/Other.pulldown/Find Tag.pushbutton/script.py
+++ ARC drawing.tab/Anotate.panel/Tool.stack/Other.pulldown/Find Tag.pushbutton/script.py
@@ -11,51 +11,51 @@ import Autodesk.Revit.DB as DB
 from System.Collections.Generic import List
 from Autodesk.Revit.UI.Selection import ObjectType
 import traceback
-uidoc = __revit__.ActiveUIDocument
-#Get Document 
-doc = uidoc.Document
-Currentview = doc.ActiveView
-t = Transaction (doc, "Get Tag of element in current view")
-t.Start()
-Ele = module.get_elements(uidoc,doc, 'Select Elements', noti = False)
-ListTag = []
-ListAnotation = []
-select = uidoc.Selection
-ListAnotationInCurrentView = []
-listid = []
-def all_elements_of_category(category):
-	return FilteredElementCollector(doc).OfCategory(category).WhereElementIsNotElementType().ToElements()
-ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_WallTags))
-ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_DoorTags))
-ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_WindowTags))
-ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_FloorTags))
-ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_StructuralColumnTags))
-ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_StructuralFramingTags))
-ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_RoomTags))
-ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_GenericModelTags))
-ListAnotation = [item for items in ListAnotation for item in items]
-CurrentviewId = Currentview.Id
-ParentView = Currentview.GetPrimaryViewId()
-for tung_tag in ListAnotation:
-    WhatView = tung_tag.OwnerViewId
-    if str(WhatView) == str(CurrentviewId):
-        ListAnotationInCurrentView.append(tung_tag)
-    elif str(WhatView) == str(ParentView):
-        ListAnotationInCurrentView.append(tung_tag)
-for i in Ele:
-    EleId = i.Id
-    for b in ListAnotationInCurrentView:
-        if b.Category.Name != "Room Tags" or b.Category.Name != "部屋タグ":
-            bhost = b.TaggedLocalElementId
-            if b.TaggedLocalElementId == EleId:
-                ListTag.append(b)
-        else:
-            if b.TaggedLocalRoomId == EleId:
-                ListTag.append (b)
-for c in ListTag:
-    listid.append(c.Id)
-Icollection = List[ElementId](listid)
-select.SetElementIds(Icollection)
-t.Commit()   
+if module.AutodeskData():
+    try:
+        uidoc = __revit__.ActiveUIDocument
+        doc = uidoc.Document
+        Currentview = doc.ActiveView
+        Ele = module.get_elements(uidoc,doc, 'Select Elements', noti = False)
+        ListTag = []
+        ListAnotation = []
+        select = uidoc.Selection
+        ListAnotationInCurrentView = []
+        listid = []
+        def all_elements_of_category(category):
+            return FilteredElementCollector(doc).OfCategory(category).WhereElementIsNotElementType().ToElements()
+        ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_WallTags))
+        ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_DoorTags))
+        ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_WindowTags))
+        ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_FloorTags))
+        ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_StructuralColumnTags))
+        ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_StructuralFramingTags))
+        ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_RoomTags))
+        ListAnotation.append(all_elements_of_category(BuiltInCategory.OST_GenericModelTags))
+        ListAnotation = [item for items in ListAnotation for item in items]
+        CurrentviewId = Currentview.Id
+        ParentView = Currentview.GetPrimaryViewId()
+        for tung_tag in ListAnotation:
+            WhatView = tung_tag.OwnerViewId
+            if str(WhatView) == str(CurrentviewId):
+                ListAnotationInCurrentView.append(tung_tag)
+            elif str(WhatView) == str(ParentView):
+                ListAnotationInCurrentView.append(tung_tag)
+        for i in Ele:
+            EleId = i.Id
+            for b in ListAnotationInCurrentView:
+                if b.Category.Name != "Room Tags" or b.Category.Name != "部屋タグ":
+                    bhost = b.TaggedLocalElementId
+                    if b.TaggedLocalElementId == EleId:
+                        ListTag.append(b)
+                else:
+                    if b.TaggedLocalRoomId == EleId:
+                        ListTag.append (b)
+        for c in ListTag:
+            listid.append(c.Id)
+        Icollection = List[ElementId](listid)
+        select.SetElementIds(Icollection)
+    except:
+        pass
 
 
