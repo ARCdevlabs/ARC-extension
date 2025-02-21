d63c9e18fc605ec106e2651dab807e7c9d910510
Nguyễn Thanh Sơn <ARCdevlabs@gmail.com>
2025-02-21 15:19:53 +0700

    Fix lỗi thêm autodesk data cho tool join

--- ARC modeling.tab/Join.panel/Select element set 1.stack/Show List 1.pushbutton/script.py
+++ ARC modeling.tab/Join.panel/Select element set 1.stack/Show List 1.pushbutton/script.py
@@ -1,31 +1,37 @@
 import pickle
-
 from pyrevit.coreutils import appdata
 from pyrevit.framework import List
 from pyrevit import revit, DB
-def get_document_data_file(file_id, file_ext, add_cmd_name=False):
-    proj_info = revit.query.get_project_info()
+import Autodesk
+import nances
+from Autodesk.Revit.DB import *
+import Autodesk.Revit.DB as DB
+from System.Collections.Generic import List
+from Autodesk.Revit.UI.Selection import ObjectType
+if nances.AutodeskData():
+    def get_document_data_file(file_id, file_ext, add_cmd_name=False):
+        proj_info = revit.query.get_project_info()
 
-    if add_cmd_name:
-        script_file_id = '{}_{}_{}'.format(EXEC_PARAMS.command_name,
-                                           file_id,
-                                           proj_info.filename
-                                           or proj_info.name)
-    else:
-        script_file_id = '{}_{}'.format(file_id,
-                                        proj_info.filename
-                                        or proj_info.name)
+        if add_cmd_name:
+            script_file_id = '{}_{}_{}'.format(EXEC_PARAMS.command_name,
+                                            file_id,
+                                            proj_info.filename
+                                            or proj_info.name)
+        else:
+            script_file_id = '{}_{}'.format(file_id,
+                                            proj_info.filename
+                                            or proj_info.name)
 
-    return appdata.get_data_file(script_file_id, file_ext)
-datafile = get_document_data_file("List1", "txt")
-selection = revit.get_selection()
-try:
-    f = open(datafile, 'r')
-    current_selection = pickle.load(f)
-    f.close()
-    element_ids = []
-    for elid in current_selection:
-        element_ids.append(DB.ElementId(int(elid)))
-    selection.set_to(element_ids)
-except Exception:
-    pass
+        return appdata.get_data_file(script_file_id, file_ext)
+    datafile = get_document_data_file("List1", "txt")
+    selection = revit.get_selection()
+    try:
+        f = open(datafile, 'r')
+        current_selection = pickle.load(f)
+        f.close()
+        element_ids = []
+        for elid in current_selection:
+            element_ids.append(DB.ElementId(int(elid)))
+        selection.set_to(element_ids)
+    except Exception:
+        pass
