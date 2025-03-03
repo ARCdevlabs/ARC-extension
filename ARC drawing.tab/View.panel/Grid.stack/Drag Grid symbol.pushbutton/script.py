84df7950de211e2c0846497023b3e5cfe4eeffb0
Thanh Tùng <166967208+tunghoanh093@users.noreply.github.com>
2025-03-03 17:34:11 +0700

    Update script.py
    Anh update lại tool rút ngắn trục+level
    1. Update khi thoát tool thì không hiện thông báo
    2. Bỏ ghi chú

--- ARC drawing.tab/View.panel/Grid.stack/Drag Grid symbol.pushbutton/script.py
+++ ARC drawing.tab/View.panel/Grid.stack/Drag Grid symbol.pushbutton/script.py
@@ -20,56 +20,45 @@ if module.AutodeskData():
 	from pyrevit.coreutils import applocales
 	current_applocale = applocales.get_current_applocale()
 
-	# Thông báo JP/VN
 	if str(current_applocale) == "日本語 / Japanese (ja)":
 		tin_nhan_0 = "通り及びレベルを切断します。"
-		tin_nhan_1 = "選択した部材なし！"
 		tin_nhan_2 = "通芯又はレベルを選択します。"
 		huong_dan_1 = "通芯又はレベルを選択するためにマウスをドラッグします。"
-		huong_dan_2 = "通芯及びレベルを選択します。"
 		huong_dan_3 = "通り長さ調整の一点をクリックして選択します。"
-		huong_dan_4 = "通芯及びレベルを再度選択して、短縮点を選択します。"
 	else:
 		tin_nhan_0 = "Cắt trục và level"
-		tin_nhan_1 = "Không có đối tượng nào được chọn!"
 		tin_nhan_2 = "Vui lòng chọn Grids hoặc Levels."
 		huong_dan_1 = "Quét chuột để chọn các Grids và Levels."
-		huong_dan_2 = "Chọn Grids và Levels."
 		huong_dan_3 = "Click chọn điểm để thay đổi chiều dài trục."    
-		huong_dan_4 = "Vui lòng chọn lại Grids và Levels. Sau đó chọn điểm rút ngắn."
 
-# Get UIDocument
 uidoc = __revit__.ActiveUIDocument
-# Get Document 
 doc = uidoc.Document
 
 class GridSelectionFilter(Autodesk.Revit.UI.Selection.ISelectionFilter):
 	def AllowElement(self, element):
 		return element.Category.Id.IntegerValue in [int(BuiltInCategory.OST_Grids), int(BuiltInCategory.OST_Levels)]
-
+	
 	def AllowReference(self, reference, point):
 		return False
 
 def pick_grid_by_rectangle():
-	try:
-		with forms.WarningBar(title=huong_dan_1):
-			selection = uidoc.Selection
-			selected_elements = selection.PickElementsByRectangle(GridSelectionFilter(), huong_dan_2)
-		return selected_elements
-	except:
-		pass
-# Lấy danh sách các đối tượng bằng cách chọn trước chạy tool sau
+	while True:
+		try:
+			with forms.WarningBar(title=huong_dan_1):
+				selection = uidoc.Selection
+				selected_elements = selection.PickElementsByRectangle(GridSelectionFilter(), tin_nhan_2)
+			if selected_elements:
+				return selected_elements
+		except Autodesk.Revit.Exceptions.OperationCanceledException:
+			sys.exit()  # Thoát lệnh nếu nhấn ESC
+
 selected_ids = uidoc.Selection.GetElementIds()
 
 if not selected_ids:
 	selected_elements = pick_grid_by_rectangle()
-	if not selected_elements:
-		module.message_box(tin_nhan_1)
-		sys.exit()
 else:
 	selected_elements = [doc.GetElement(id) for id in selected_ids]
 
-# Lọc ra các đối tượng Grid và Level
 grids = []
 levels = []
 
@@ -79,36 +68,31 @@ for element in selected_elements:
 	elif isinstance(element, Level):
 		levels.append(element)
 
-# Nếu không có Grid hoặc Level nào được chọn, hiển thị thông báo và thoát
-if not grids and not levels:
-	module.message_box(tin_nhan_2)
-	sys.exit()
+while not grids and not levels:
+	selected_elements = pick_grid_by_rectangle()
+	grids = [el for el in selected_elements if isinstance(el, Grid)]
+	levels = [el for el in selected_elements if isinstance(el, Level)]
 
-# Hàm thiết lập work plane
 def set_work_plane_for_view(view):
 	try:
-		# Create a Plane using the view's direction and origin
 		plane = Autodesk.Revit.DB.Plane.CreateByNormalAndOrigin(view.ViewDirection, view.Origin)
 		sketch_plane = Autodesk.Revit.DB.SketchPlane.Create(doc, plane)
 		view.SketchPlane = sketch_plane
 		return True
 	except:
-		# print("Lỗi khi set Work Plane: ", e)
-		# print(traceback.format_exc())
 		return False
 
-# Hàm click chuột
 def pick_point_with_nearest_snap(iuidoc):
 	snap_settings = Autodesk.Revit.UI.Selection.ObjectSnapTypes.None
 	prompt = huong_dan_3
-	click_point = None
 	try:
 		click_point = iuidoc.Selection.PickPoint(snap_settings, prompt)
+		return click_point
+	except Autodesk.Revit.Exceptions.OperationCanceledException:
+		sys.exit()  # Thoát lệnh nếu nhấn ESC
 	except Exception:
-		pass  # Ignore the exception and continue
-	return click_point
+		return None
 
-# Hàm xác định vị trí chuột so với đầu trục
 def nearest_point_on_line(start, end, point):
 	line_direction = (end - start).Normalize()
 	vector = point - start
@@ -116,8 +100,7 @@ def nearest_point_on_line(start, end, point):
 	closest_point = start + line_direction * distance
 	return closest_point
 
-# Rút ngắn trục ở mb
-def RUTNGAN_TRUC_MAT_BANG(grid, view, click_point):
+def RUTNGAN_TRUC(grid, view, click_point):
 	datum_extent_type = Autodesk.Revit.DB.DatumExtentType.ViewSpecific
 	list_curve = grid.GetCurvesInView(datum_extent_type, view)
 	if list_curve:
@@ -126,53 +109,17 @@ def RUTNGAN_TRUC_MAT_BANG(grid, view, click_point):
 			start_point = curve.GetEndPoint(0)
 			end_point = curve.GetEndPoint(1)
 
-			# Tính điểm gần nhất với click_point
 			closest_point = nearest_point_on_line(start_point, end_point, click_point)
 			distance_to_start = closest_point.DistanceTo(start_point)
 			distance_to_end = closest_point.DistanceTo(end_point)
 
-			# Tính điểm new start and end points
-			if distance_to_start < distance_to_end:
-				new_start_point = closest_point
-				new_end_point = end_point
-			else:
-				new_start_point = start_point
-				new_end_point = closest_point
+			new_start_point = closest_point if distance_to_start < distance_to_end else start_point
+			new_end_point = end_point if distance_to_start < distance_to_end else closest_point
 
-			# Tạo curve mới với tọa độ mới
 			new_curve = Line.CreateBound(new_start_point, new_end_point)
 			if new_curve.IsBound:
 				grid.SetCurveInView(datum_extent_type, view, new_curve)
 
-# Rút ngắn trục mặt đứng
-def RUTNGAN_TRUC_MAT_DUNG(grid, view, click_point):
-	datum_extent_type = Autodesk.Revit.DB.DatumExtentType.ViewSpecific
-	list_curve = grid.GetCurvesInView(datum_extent_type, view)
-	if list_curve:
-		curve = list_curve[0]
-		if isinstance(curve, Line):
-			start_point = curve.GetEndPoint(0)
-			end_point = curve.GetEndPoint(1)
-
-			# Tính điểm gần nhất với click_point
-			closest_point = nearest_point_on_line(start_point, end_point, click_point)
-			distance_to_start = closest_point.DistanceTo(start_point)
-			distance_to_end = closest_point.DistanceTo(end_point)
-
-			# So sánh vị trí new start and end points
-			if distance_to_start < distance_to_end:
-				new_start_point = closest_point
-				new_end_point = end_point
-			else:
-				new_start_point = start_point
-				new_end_point = closest_point
-
-			# Tạo curve mới với tọa độ mới
-			new_curve = Line.CreateBound(new_start_point, new_end_point)
-			if new_curve.IsBound:
-				grid.SetCurveInView(datum_extent_type, view, new_curve)
-
-# Rút ngắn level
 def RUTNGAN_LEVEL(level, click_point):
 	datum_extent_type = Autodesk.Revit.DB.DatumExtentType.ViewSpecific
 	list_curve = level.GetCurvesInView(datum_extent_type, doc.ActiveView)
@@ -182,19 +129,13 @@ def RUTNGAN_LEVEL(level, click_point):
 			start_point = curve.GetEndPoint(0)
 			end_point = curve.GetEndPoint(1)
 
-			# Tính toán điểm gần nhất
 			closest_point = nearest_point_on_line(start_point, end_point, click_point)
 			distance_to_start = closest_point.DistanceTo(start_point)
 			distance_to_end = closest_point.DistanceTo(end_point)
 
-			if distance_to_start < distance_to_end:
-				new_start_point = closest_point
-				new_end_point = end_point
-			else:
-				new_start_point = start_point
-				new_end_point = closest_point
+			new_start_point = closest_point if distance_to_start < distance_to_end else start_point
+			new_end_point = end_point if distance_to_start < distance_to_end else closest_point
 
-			# Vẽ lại curve mới
 			new_curve = Line.CreateBound(new_start_point, new_end_point)
 			if new_curve.IsBound:
 				level.SetCurveInView(datum_extent_type, doc.ActiveView, new_curve)
@@ -203,7 +144,6 @@ trans_group = TransactionGroup(doc, tin_nhan_0)
 trans_group.Start()
 
 try:
-	# Chạy set work plane
 	t1 = Transaction(doc, "Set Work Plane")
 	t1.Start()
 	if not set_work_plane_for_view(uidoc.ActiveView):
@@ -213,31 +153,23 @@ try:
 		sys.exit()
 	t1.Commit()
 
-	# Chọn vị trí thay đổi chiều dài
 	with forms.WarningBar(title=huong_dan_3):
 		click_point = pick_point_with_nearest_snap(uidoc)
 
 	if not click_point:
-		module.message_box(huong_dan_4)
+		module.message_box("Không có điểm nào được chọn.")
 		trans_group.RollBack()
 		sys.exit()
 
-	# Thay đổi chiều dài trục và level
 	t2 = Transaction(doc, tin_nhan_0)
 	t2.Start()
 	for grid in grids:
-		if doc.ActiveView.ViewType == ViewType.FloorPlan or doc.ActiveView.ViewType == ViewType.CeilingPlan:
-			RUTNGAN_TRUC_MAT_BANG(grid, doc.ActiveView, click_point)
-		else:
-			RUTNGAN_TRUC_MAT_DUNG(grid, doc.ActiveView, click_point)
+		RUTNGAN_TRUC(grid, doc.ActiveView, click_point)
 	for level in levels:
 		RUTNGAN_LEVEL(level, click_point)
 	t2.Commit()
 
 	trans_group.Assimilate()
 except:
-	# print(traceback.format_exc())
-# 	trans_group.RollBack()
-# finally:
-# 	trans_group.Dispose()
+	trans_group.RollBack()
 	sys.exit()
