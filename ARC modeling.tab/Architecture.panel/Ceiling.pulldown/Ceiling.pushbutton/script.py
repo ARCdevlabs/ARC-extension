6ace4bbf9e3fe49a4e90333b8616515bfc91cb89
Nguyễn Thanh Sơn <ARCdevlabs@gmail.com>
2026-03-10 09:57:37 +0700

    Thêm tool Mwread Mwrite

--- ARC modeling.tab/Architecture.panel/Ceiling.pulldown/Ceiling.pushbutton/script.py
+++ ARC modeling.tab/Architecture.panel/Ceiling.pulldown/Ceiling.pushbutton/script.py
@@ -1,11 +1,6 @@
+# -*- coding: utf-8 -*-
 __doc__ = 'python for revit api'
 __author__ = 'NguyenThanhSon' "Email: nguyenthanhson1712@gmail.com"
-import string
-import importlib
-
-ARC = string.ascii_lowercase
-begin = "".join(ARC[i] for i in [13, 0, 13, 2, 4, 18])
-module = importlib.import_module(str(begin))
 import Autodesk
 from Autodesk.Revit.DB import *
 from System.Collections.Generic import *
@@ -14,8 +9,9 @@ import sys
 from Autodesk.Revit.UI.Selection import ObjectType
 from Autodesk.Revit.UI import UIDocument
 from rpw.ui.forms import Alert
+import nances
 try:
-    if module.AutodeskData():
+    if nances.AutodeskData():
         uidoc = __revit__.ActiveUIDocument
         doc = uidoc.Document
         pick = uidoc.Selection.PickObjects(ObjectType.Element)
@@ -23,90 +19,147 @@ try:
         for i in pick:
             element_id = i.ElementId
             covert_reference_to_element.append(doc.GetElement(element_id))
-        # Ele = module.get_selected_elements(uidoc,doc) #Code nay de chon truoc room
+            
         Rooms = covert_reference_to_element
+        active_view = doc.ActiveView # Lấy View hiện tại để vẽ Detail Line
         try: 
-            active_level = doc.ActiveView.GenLevel
+            active_level = active_view.GenLevel
             active_level_Id = active_level.Id
         except:
             Alert('Please run this tool on plan view and just select rooms',title="ARC tools",header= "")
             sys.exit()
+            
         t = Transaction (doc, "Create ceiling from room")
         t.Start()
-        def create_ceilings(rooms, ceil_type, offset, level_Id):
+        
+        failed_rooms = []
+
+        # HÀM MỚI: Vẽ Detail Line bao quanh phòng
+        def draw_detail_lines(room, view):
+            options = SpatialElementBoundaryOptions()
+            options.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Finish
+            boundaries = room.GetBoundarySegments(options)
+            if boundaries:
+                for boundary_list in boundaries:
+                    for segment in boundary_list:
+                        curve = segment.GetCurve()
+                        try:
+                            doc.Create.NewDetailCurve(view, curve)
+                        except:
+                            pass # Bỏ qua nếu có đoạn line siêu nhỏ không thể vẽ
+
+        def create_ceilings(rooms, ceil_type, offset, level_Id, draw_lines_if_failed):
             ceilings = []
             for room in rooms:
-                # IGNORE NON-BOUNDING ROOMS
                 if not room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble():
-                    return None
+                    continue
 
-                # ROOM BOUNDARIES -> List[CurveLoop]()
-                room_boundaries = room.GetBoundarySegments(SpatialElementBoundaryOptions())
-                curveLoopList   = List[CurveLoop]()
+                curveLoopList = None
 
-                for roomBoundary in room_boundaries:
-                    room_curve_loop = CurveLoop()
-                    for boundarySegment in roomBoundary:
-                        curve = boundarySegment.GetCurve()
-                        room_curve_loop.Append(curve)
-                    curveLoopList.Add(room_curve_loop)
+                # Lấy mặt đáy từ khối Solid 3D
+                geom_elem = room.ClosedShell
+                if geom_elem:
+                    for geom_obj in geom_elem:
+                        if isinstance(geom_obj, Solid) and geom_obj.Faces.Size > 0:
+                            for face in geom_obj.Faces:
+                                if isinstance(face, PlanarFace):
+                                    if face.FaceNormal.IsAlmostEqualTo(XYZ(0, 0, -1)):
+                                        curveLoopList = face.GetEdgesAsCurveLoops()
+                                        break
+                            if curveLoopList:
+                                break
+                
+                if not curveLoopList:
+                    options = SpatialElementBoundaryOptions()
+                    options.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Finish
+                    room_boundaries = room.GetBoundarySegments(options)
+                    
+                    if room_boundaries:
+                        curveLoopList = List[CurveLoop]()
+                        for roomBoundary in room_boundaries:
+                            curve_list = List[Curve]()
+                            for boundarySegment in roomBoundary:
+                                curve_list.Add(boundarySegment.GetCurve())
+                            try:
+                                room_curve_loop = CurveLoop.Create(curve_list)
+                                curveLoopList.Add(room_curve_loop)
+                            except:
+                                pass
 
-                #  CREATE CEILINGS
-                if curveLoopList:
-                    ceiling = Autodesk.Revit.DB.Ceiling.Create(doc, curveLoopList, ceil_type, level_Id)
-                    ceilings.append(ceiling)
-                    # SET OFFSET
-                    param = ceiling.get_Parameter(BuiltInParameter.CEILING_HEIGHTABOVELEVEL_PARAM)
-                    param.Set(offset)
+                success = False
+                if curveLoopList and curveLoopList.Count > 0:
+                    try:
+                        ceiling = Autodesk.Revit.DB.Ceiling.Create(doc, curveLoopList, ceil_type, level_Id)
+                        ceilings.append(ceiling)
+                        # SET OFFSET
+                        param = ceiling.get_Parameter(BuiltInParameter.CEILING_HEIGHTABOVELEVEL_PARAM)
+                        param.Set(offset)
+                        success = True
+                    except Exception as e:
+                        pass
+                
+                if not success:
+                    failed_rooms.append(room.Id.IntegerValue)
+                    # Thực thi vẽ Detail Line nếu người dùng chọn tùy chọn này
+                    if draw_lines_if_failed:
+                        draw_detail_lines(room, active_view)
 
             return ceilings
-
 
         def all_type_of_ceiling():
             all_ceiling_type = FilteredElementCollector(doc).OfClass(CeilingType).OfCategory(BuiltInCategory.OST_Ceilings)
             return all_ceiling_type
+            
         list_ceiling = all_type_of_ceiling()
-    
+
+        from rpw.ui.forms import (FlexForm, Label, ComboBox, TextBox, CheckBox, Separator, Button)
         
-        # from pyrevit import forms   (khong thich dung form cua pyrevit nua)
-        # selected_ceiling = \
-        #     forms.CommandSwitchWindow.show(
-        #         sorted([Autodesk.Revit.DB.Element.Name.GetValue(x) for x in list_ceiling]),
-        #         message='Pick type of ceiling:'
-        #     )
-
-
-        from rpw.ui.forms import (FlexForm, Label, ComboBox, TextBox, TextBox,
-                                    Separator, Button, CheckBox)
         components = [Label('Select type of ceiling:'),
                         ComboBox('combobox1', [Autodesk.Revit.DB.Element.Name.GetValue(x) for x in list_ceiling]),
                         Label('Ceiling height:'),
                         TextBox('textbox1', Text="2700"),
-                    #   CheckBox('checkbox1', 'Check this'), (khong can check box)
+                        CheckBox('draw_lines_cb', 'Draw Detail Lines for failed rooms', default=True),
                         Separator(),
                         Button('Create ceiling')]
+                        
         form = FlexForm('ARC tools', components)
         form.show()
-        # User selects `Opt 1`, types 'Wood' in TextBox, and select Checkbox
-        form.values
-        selected_ceiling = form.values["combobox1"]
-        ceiling_height = float(form.values["textbox1"])
-        list_new_ceiling = []
-        for i in list_ceiling:
-            try:
-                type_name = Autodesk.Revit.DB.Element.Name.GetValue(i)
-                if type_name == selected_ceiling:
-                    type_Id = i.Id
-                    list_new_ceiling = create_ceilings(Rooms, type_Id, ceiling_height/304.8, active_level_Id)
-            except:
-                pass
-        t.Commit()
-        select = uidoc.Selection
-        list_id = []
-        for i in list_new_ceiling:
-            ceiling_id = i.Id
-            list_id.append(ceiling_id)
-        Icollection = List[ElementId](list_id)
-        select.SetElementIds(Icollection)
-except:
+        
+        if form.values:
+            selected_ceiling = form.values["combobox1"]
+            ceiling_height = float(form.values["textbox1"])
+            draw_lines_if_failed = form.values["draw_lines_cb"] # Lấy giá trị True/False từ ô tick
+            list_new_ceiling = []
+            
+            for i in list_ceiling:
+                try:
+                    type_name = Autodesk.Revit.DB.Element.Name.GetValue(i)
+                    if type_name == selected_ceiling:
+                        type_Id = i.Id
+                        # Truyền thêm biến draw_lines_if_failed vào hàm
+                        list_new_ceiling = create_ceilings(Rooms, type_Id, ceiling_height/304.8, active_level_Id, draw_lines_if_failed)
+                        break
+                except:
+                    pass
+            
+            t.Commit()
+            
+            if list_new_ceiling:
+                select = uidoc.Selection
+                list_id = []
+                for i in list_new_ceiling:
+                    ceiling_id = i.Id
+                    list_id.append(ceiling_id)
+                Icollection = List[ElementId](list_id)
+                select.SetElementIds(Icollection)
+                
+            if failed_rooms:
+                failed_str = ", ".join(str(id) for id in failed_rooms)
+                msg = 'Có một số room bị lỗi hình học, không thể tạo trần.\nDanh sách Room ID: ' + failed_str
+                if draw_lines_if_failed:
+                    msg += '\n\nAdd-in đã tự động vẽ Detail Lines bao quanh các phòng này để bạn dễ dàng rà soát và sửa lỗi.'
+                Alert(msg, title="ARC tools", header="Cảnh báo")
+        else:
+            t.RollBack()
+except Exception as e:
     pass
---
+++ ARC.tab/Selection.panel/Column3.stack/MAppend.pulldown/MRead 2.pushbutton/script.py
@@ -0,0 +1,24 @@
+import pickle
+
+from pyrevit.framework import List
+from pyrevit import script
+from pyrevit import revit, DB
+
+
+selection = revit.get_selection()
+
+datafile = script.get_document_data_file("Memory_2", "txt")
+
+
+try:
+    f = open(datafile, 'r')
+    current_selection = pickle.load(f)
+    f.close()
+
+    element_ids = []
+    for elid in current_selection:
+        element_ids.append(DB.ElementId(int(elid)))
+
+    selection.set_to(element_ids)
+except Exception:
+    pass
